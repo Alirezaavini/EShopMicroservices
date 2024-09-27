b90c8b667128fbcdbc4294664643b170687ff4d2
AlirezaAvini <alirezaavini751@gmail.com>
2024-09-27 18:45:10 +0330

    careter and mapster

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
 
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
+// ----------- builder
 var app = builder.Build();
 
+app.MapCarter();
 
 app.Run();
